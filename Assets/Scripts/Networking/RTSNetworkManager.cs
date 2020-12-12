f9ac2e889d778fa6e5b9b02e8861479721bc67c2
dnasol <denzelnasol@gmail.com>
2020-12-11 22:13:01 -0800

    Unity initial commit

--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,18 +1,9 @@
-﻿using System.Collections;
+﻿using Mirror;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RTSNetworkManager : MonoBehaviour
+public class RTSNetworkManager : NetworkManager
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
