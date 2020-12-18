b4b8f386a429f647973f1f0a61a07307eaee1522
dnasol <denzelnasol@gmail.com>
2020-12-18 13:21:13 -0800

    Revert "Add publisher/subscriber pattern for list of units for client and server"
    This reverts commit dd17e2844d2fd689275c0a3993c8a2b8260b3692.

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using Mirror;
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,46 +10,12 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
 
-    public static event Action<Unit> ServerOnUnitSpawned;
-    public static event Action<Unit> ServerOnUnitDespawned;
-
-    public static event Action<Unit> AuthorityOnUnitSpawned;
-    public static event Action<Unit> AuthorityOnUnitDespawned;
-
     public UnitMovement GetUnitMovement()
     {
         return unitMovement;
     }
 
-    #region Server
-
-    public override void OnStartServer()
-    {
-        ServerOnUnitSpawned?.Invoke(this);
-    }
-
-    public override void OnStopServer()
-    {
-        ServerOnUnitDespawned?.Invoke(this);
-    }
-
-    #endregion
-
     #region Client
-
-    public override void OnStartClient()
-    {
-        if (!isClientOnly || !hasAuthority) { return; }
-
-        AuthorityOnUnitSpawned?.Invoke(this);
-    }
-
-    public override void OnStopClient()
-    {
-        if (!isClientOnly || !hasAuthority) { return; }
-
-        AuthorityOnUnitDespawned?.Invoke(this);
-    }
 
     [Client]
     public void Select()
