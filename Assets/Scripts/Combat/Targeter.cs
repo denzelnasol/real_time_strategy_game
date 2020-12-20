53c7101baaa5020cb185018c2b85af73dd8180d7
dnasol <denzelnasol@gmail.com>
2020-12-20 11:23:41 -0800

    Add targeting to unit

---
+++ Assets/Scripts/Combat/Targeter.cs
@@ -0,0 +1,33 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Targeter : NetworkBehaviour
+{
+    [SerializeField] private Targetable target;
+
+    #region Server
+
+    [Command]
+    public void CmdSetTarget(GameObject targetGameObject)
+    {
+        if (!targetGameObject.TryGetComponent<Targetable>(out Targetable newTarget)) { return; }
+
+        this.target = newTarget;
+    }
+
+    [Server]
+    public void ClearTarget()
+    {
+        target = null;
+    }
+
+    #endregion
+
+    #region Client
+
+    
+
+    #endregion
+}
