a40cb156d7114416b93143fa0f259bb9483fea86
dnasol <denzelnasol@gmail.com>
2020-12-21 16:49:35 -0800

    Add game over screen

--- Assets/Scripts/Buildings/GameOverHandler.cs
+++ Assets/Scripts/Buildings/GameOverHandler.cs
@@ -1,10 +1,13 @@
 ﻿using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOverHandler : NetworkBehaviour
 {
+
+    public static event Action<string> ClientOnGameOver;
 
     private List<UnitBase> bases = new List<UnitBase>();
 
@@ -35,12 +38,20 @@ public class GameOverHandler : NetworkBehaviour
 
         if (bases.Count != 1) { return; }
 
-        Debug.Log("Game over");
+        int playerId = bases[0].connectionToClient.connectionId;
+
+        RpcGameOver($"Player {playerId}");
     }
 
     #endregion
 
     #region Client
+
+    [ClientRpc]
+    private void RpcGameOver(string winner)
+    {
+        ClientOnGameOver?.Invoke(winner);
+    }
 
     #endregion
 }
